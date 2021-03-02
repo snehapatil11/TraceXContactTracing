fb85ed910c0cd0f37767494f4dbe430d09557497
Juhi Nayak <juhinayak07@gmail.com>
2021-03-01 22:22:04 -0800

    Generate UUID for device

--- app/src/main/java/com/example/tracexcontacttracing/bottomnav/fragments/NotificationFragment.kt
+++ app/src/main/java/com/example/tracexcontacttracing/bottomnav/fragments/NotificationFragment.kt
@@ -1,60 +1,92 @@
 package com.example.tracexcontacttracing.bottomnav.fragments
 
+import android.content.Context
+import android.media.audiofx.BassBoost
+import android.media.audiofx.Equalizer
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import com.example.tracexcontacttracing.R
+import com.example.tracexcontacttracing.notification.FirebaseService
+import com.example.tracexcontacttracing.notification.NotificationData
+import com.example.tracexcontacttracing.notification.PushNotification
+import com.example.tracexcontacttracing.notification.RetrofitInstance
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.fragment_notification.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NotificationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NotificationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+const val TOPIC = "TraceXNotificationTopic"
+class NotificationFragment : Fragment(), View.OnClickListener {
+    val TAG = "Notification"
+    private val uniqueID = UUID.randomUUID().toString()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+
+        val view: View = inflater!!.inflate(R.layout.fragment_notification, container, false)
+        val btn: Button = view.findViewById(R.id.btnSend)
+        btn.setOnClickListener(this)
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotificationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotificationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+        fun newInstance(): NotificationFragment {
+            return NotificationFragment()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        btnSend.setOnClickListener {
+            val title = etTitle.text.toString()
+            val message = etMessage.text.toString()
+            val recipientToken = etToken.text.toString()
+            if(title.isNotEmpty() && message.isNotEmpty() && recipientToken.isNotEmpty()) {
+                PushNotification(
+                    NotificationData(title, message),
+                    recipientToken
+                ).also {
+                    sendNotification(it)
                 }
             }
+        }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("Device Unique UUID is: $uniqueID")
+        FirebaseService.sharedPref = getActivity()?.getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener {
+            FirebaseService.token = it.token
+            etToken.setText(it.token)
+        }
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+    }
+
+    private fun sendNotification(notification: PushNotification) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val response = RetrofitInstance.api.postNotification(notification)
+            if(response.isSuccessful) {
+                Log.d(TAG, "Response: ${Gson().toJson(response)}")
+            } else {
+                Log.e(TAG, response.errorBody().toString())
+            }
+        } catch(e: Exception) {
+            Log.e(TAG, e.toString())
+        }
+    }
+
+
 }
