dac6731d679f314093d8ddb6adbf7c70524e2ec5
Chi Hoang <chhoang102@gmail.com>
2021-03-12 18:07:49 -0800

    demo roomdb in checkin fragment

--- app/src/main/java/com/example/tracexcontacttracing/bottomnav/fragments/CheckinFragment.kt
+++ app/src/main/java/com/example/tracexcontacttracing/bottomnav/fragments/CheckinFragment.kt
@@ -1,11 +1,18 @@
 package com.example.tracexcontacttracing.bottomnav.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.tracexcontacttracing.GetData
 import com.example.tracexcontacttracing.R
+import com.example.tracexcontacttracing.data.DeviceEntity
+import com.example.tracexcontacttracing.database.RoomDb
+import kotlinx.android.synthetic.main.fragment_checkin.*
+import kotlinx.android.synthetic.main.fragment_checkin.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,8 +41,14 @@ class CheckinFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_checkin, container, false)
+
+        view.saveButton.setOnClickListener {
+            saveData()
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkin, container, false)
+        return view
     }
 
     companion object {
@@ -56,5 +69,27 @@ class CheckinFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    fun saveData() {
+//        val deviceToken: EditText = findViewById(R.id.editViewDeviceToken)
+//        val deviceId: EditText = findViewById(R.id.editViewDeviceId)
+
+        val deviceTokenText = editViewDeviceToken.text.toString().trim()
+        val deviceIdText = editViewDeviceId.text.toString().trim()
+
+        // TODO check input
+        val device = DeviceEntity(0, deviceTokenText, deviceIdText, System.currentTimeMillis(), System.currentTimeMillis())
+
+        val deviceDao = RoomDb.getAppDatabase(this.context!!)?.deviceDao()
+        val id = deviceDao?.insert(device)
+
+        println("saved user $device with id=$id")
+
+//        Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show()
+
+        val intent: Intent = Intent(getActivity(), GetData::class.java);
+        startActivity(intent);
+//        startActivity(Intent(this, GetData::class.java))
     }
 }
