e7812f5903c7fabd7bb9a23dafcfdcf84e1d7acb
Chi Hoang <chhoang102@gmail.com>
2021-04-21 00:45:33 -0700

    add checkin record in roomdb

---
+++ app/src/main/java/com/example/tracexcontacttracing/dao/CheckinRecordDao.kt
@@ -0,0 +1,19 @@
+package com.example.tracexcontacttracing.dao
+
+import androidx.room.*
+import com.example.tracexcontacttracing.data.CheckinRecordEntity
+
+@Dao
+interface CheckinRecordDao {
+    @Query("SELECT * FROM checkin_record")
+    fun getAllCheckinRecordEntities(): List<CheckinRecordEntity>?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(checkinRecord: CheckinRecordEntity): Long
+
+    @Delete
+    fun delete(checkinRecord: CheckinRecordEntity): Int
+
+    @Update
+    fun update(checkinRecord: CheckinRecordEntity)
+}
---
+++ app/src/main/java/com/example/tracexcontacttracing/data/CheckinRecordEntity.kt
@@ -0,0 +1,17 @@
+package com.example.tracexcontacttracing.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "checkin_record")
+data class CheckinRecordEntity(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "record_id") val recordId: Int,
+    @ColumnInfo(name = "device_token") val fever: Boolean,
+    @ColumnInfo(name = "cough") val cough: Boolean,
+    @ColumnInfo(name = "taste_loss") val tasteLoss: Boolean,
+    @ColumnInfo(name = "sore_throat") val soreThroat: Boolean,
+    @ColumnInfo(name = "created_at") val createdAt: Long,
+    @ColumnInfo(name = "modified_at") val modifiedAt: Long
+) {
+}
