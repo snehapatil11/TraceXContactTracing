8f5a44b32e51fb48a5cb42b271e0c67b0356479c
Rashmi Sarode <rashmisarode92@gmail.com>
2021-04-25 23:32:51 -0700

    adding chart tab adapter

--- app/src/main/java/com/example/tracexcontacttracing/fragment/tabs/ChartTabsAdapterV2.kt
+++ app/src/main/java/com/example/tracexcontacttracing/fragment/tabs/ChartTabsAdapterV2.kt
@@ -1,2 +1,27 @@
 package com.example.tracexcontacttracing.fragment.tabs
 
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ChartTabsAdapterV2(fragment: Fragment): FragmentStateAdapter(fragment) {
+
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList = ArrayList<String>()
+
+    override fun createFragment(position: Int): Fragment {
+        return mFragmentList.get(position)
+    }
+
+    override fun getItemCount(): Int {
+        return mFragmentList.size
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+
+    fun getTitle(position: Int): String {
+       return mFragmentTitleList.get(position)
+    }
+}
